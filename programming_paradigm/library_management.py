58b40eff202c03749f364e8b10311bd94a382254
MohamedKhaliel <mohamedahmed201131@gmail.com>
2024-10-06 15:22:21 +0300

    add

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,39 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title 
+        self.author = author
+        self._is_checked_out = False
+        
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+       
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book._is_checked_out = True
+                return f"{title} is found"
+            else:
+                return f"{title} is not found"
+    
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title :
+                self._books.remove(book)
+
+    def list_available_books(self):
+        for book in self._books:
+            print(f"{book.title} by {book.author}")
