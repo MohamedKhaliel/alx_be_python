61f8d91b704b5e185c7671ab1d9c0f71b3b28cf7
MohamedKhaliel <mohamedahmed201131@gmail.com>
2024-10-13 18:56:43 +0300

    polymo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+# Base Class
+class Shape:
+    def area(self):
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+# Derived Class - Rectangle
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+# Derived Class - Circle
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
